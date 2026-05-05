8669271e565fad8f83a2f30f292c81507fc6f0e3
Renzo Nodari <renzonodari9@gmail.com>
2026-05-05 14:23:32 -0300

    Fix: import index.css with Tailwind in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './style.css'
-import './responsive.css'
+import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
